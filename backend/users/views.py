e3c8d3cd9e6376fe1ca8092e59cf55f9bd146969
jorbort <jorbort92@gmail.com>
2024-09-25 13:22:10 +0200

    agregada las vista para refrescar los tokens de authentificacion

--- backend/users/views.py
+++ backend/users/views.py
@@ -9,9 +9,10 @@ from django.contrib.auth import logout as django_logout
 from django.core.mail import send_mail
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from django.views.decorators.csrf import csrf_exempt
+
 from rest_framework import status, generics
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
 from .models import PongUser , MatchHistory ,Friendship
 from .serializer import UserSerializer , MatcHistorySerializer, FrienshipSerializer
 
@@ -116,6 +117,29 @@ def logout_view(request):
 	response.delete_cookie('csrftoken')
 	
 	return response
+
+@api_view(['POST'])
+def refreshTokens(request):
+	refresh_token = request.data.get('refresh_token')
+
+	if refresh_token in None:
+		return Response({'detail': 'Refresh token is required.'}, status=status.HTTP_400_BAD_REQUEST)
+	
+	try:
+		refresher = RefreshToken(refresh_token)
+		new_access_token = str(refresher.access_token)
+		new_refresh_token = str(refresher)
+
+		response = Response({
+            'access_token': new_access_token,
+            'refresh_token': new_refresh_token
+        }, status=status.HTTP_200_OK)
+		response.set_cookie('access_token', new_access_token)
+		response.set_cookie('refresh_token', new_refresh_token)
+
+		return response
+	except TokenError as e:
+		return Response({'detail' : 'Invalid refresh token'}, status= status.HTTP_401_UNAUTHORIZED)
 
 @api_view(['GET'])
 def match_history(request):
