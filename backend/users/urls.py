d85b0fcd1a932ca5e1fc4162c53d99fff55e85cb
jorbort <jorbort92@gmail.com>
2024-10-22 00:22:20 +0200

    limpieza

--- backend/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from users import views
-
-urlpatterns = [
-	path('', views.hello_world),
-	path('create', views.addUser),
-	path('read/<str:pk>', views.getUser),
-	path('listAll', views.getUsers)
-]
