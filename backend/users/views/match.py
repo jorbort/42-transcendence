00ed27f510c47042d982d0d3d3396d08be90fa8e
Jorge Bortolotti Suarez <jbortolo@car7s2.42barcelona.com>
2024-12-06 16:02:26 +0100

    hechos algunos cambios en el registro de partidos para que se hagan con el username y no con el user id

--- backend/users/views/match.py
+++ backend/users/views/match.py
@@ -2,30 +2,53 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from users.models import MatchHistory
+from users.models import MatchHistory , PongUser
 from users.serializer import MatcHistorySerializer
 
 
 @api_view(['GET'])
-# @permission_classes([IsAuthenticated])
+@permission_classes([IsAuthenticated])
 def match_history(request):
-	if request.user.is_authenticated:
-		matches_as_player1= MatchHistory.objects.filter(player1=request.user)
-		matches_as_player2=MatchHistory.objects.filter(player2=request.user) 
-		matches= matches_as_player1 | matches_as_player2
-		serializer = MatcHistorySerializer(matches, many=True)
-		return Response(serializer.data, status=status.HTTP_200_OK)
-	else:
-		return Response({'detail': 'Authentication credentials were not provided'})
+	username = request.query_params.get('username',None)
+	if not username:
+		return Response({'detail': 'Username parameter is required'}, status=status.HTTP_400_BAD_REQUEST)
+	try:
+		user = PongUser.objects.get(username=username)
+	except PongUser.DoesNotExist:
+		return Response({'detail': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+	
+	matches_as_player1= MatchHistory.objects.filter(player1=user)
+	matches_as_player2=MatchHistory.objects.filter(player2=user) 
+	matches= matches_as_player1 | matches_as_player2
+	serializer = MatcHistorySerializer(matches, many=True)
+	return Response(serializer.data, status=status.HTTP_200_OK)
+	
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def record_match(request):
-	if request.user.is_authenticated:
-		serializer= MatcHistorySerializer(data=request.data)
+		player1_username= request.data.get('player1')
+		player2_username=request.data.get('player2')
+		winner_username = request.data.get('winner')
+		tournament_id = request.data.get('tournament_id')
+
+		try:
+			player1 = PongUser.objects.get(username=player1_username)
+			player2 = PongUser.objects.get(username=player2_username)
+			winner = PongUser.objects.get(username=winner_username)
+		except: PonUser.DoesNotexist:
+			return response({'detail': 'One or both users not found'}, status=status.HTTP_400_BAD_REQUEST)
+
+		match_data = {
+            'player1': player1.id,
+            'player2': player2.id,
+            'player1_score': request.data.get('player1_score'),
+        	'player2_score': request.data.get('player2_score'),
+			'winner': winner.id,
+        	'tournament_id': tournament_id
+        }
+		serializer= MatcHistorySerializer(data=request_data)
 		if serializer.is_valid():
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-	else:
-		return Response({'detail': 'authentication credential where not provided'})
