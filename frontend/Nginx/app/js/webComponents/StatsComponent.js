7509ab0da9eec736ebe5ddf55231ab87bfeed803
Juan Antonio Martínez Morales <juan-anm@car9s4.42barcelona.com>
2024-12-09 19:23:47 +0100

    stats pie chart y lista partidos implementada falta grafico barras

--- frontend/Nginx/app/js/webComponents/StatsComponent.js
+++ frontend/Nginx/app/js/webComponents/StatsComponent.js
@@ -10,6 +10,18 @@ class GameStats extends HTMLElement {
 				height: 10px;
 				width: 10px;
 			}
+			.nogamehistory {
+				color: rgba(160, 215, 160, 0.9);
+				font-size: 1.5rem;
+				font-family: 'Press Start 2P', cursive;
+				text-align: center;
+				margin-top: 10%;
+			}
+			.table-container {
+                width: 100%;
+                max-height: 35%; /* Ajusta la altura máxima según sea necesario */
+                overflow-y: auto; /* Habilita el scroll vertical */
+            }
 			.GameStatsContainer {
 				font-family: 'Press Start 2P', cursive;
 				font-size: 0.8rem;
@@ -17,10 +29,10 @@ class GameStats extends HTMLElement {
 				background-color: #2b3339;
 				display: flex;
 				flex-direction: column;
-				justify-content: flex-start;
+				justify-content: flex-end;
 				align-items: center;
 				width: 79vw;
-				height: 60vh;
+				height: 70vh;
 				position: fixed;
 				top: 60%;
 				transform: translateY(-50%);
@@ -28,12 +40,6 @@ class GameStats extends HTMLElement {
 				border-radius: 30px;
 				box-shadow: 0 0 10px 2px rgba(0,0,0,0.5);
 			}
-			//.GameStatsContainer:hover {
-			//	height: 60vh;
-			//	width: 25vw;
-			//	box-shadow: 0 0 15px 3px rgba(0,15,5,0.8);
-			//	transition: all 0.5s ease-in-out;
-			//}
 			.GameStats-div {
 				color: rgba(160, 215, 160, 0.9);
 				margin: 4px;
@@ -45,12 +51,6 @@ class GameStats extends HTMLElement {
 				box-shadow: 0 0 5px 0 rgba(0,0,0,0.3);
 				border-radius: 10px;
 			}
-			// .GameStats-div:hover {
-			//	color: rgba(200, 225, 200, 0.7);
-			//	box-shadow: 0 0 8px 3px rgba(0, 20, 5, 0.8);
-			//	transform: translatey(-1px);
-			//	transition: all 0.1s ease-in-out;
-			// }
 			.statsContainer {
 				width: 100%;
 				display: flex;
@@ -61,98 +61,160 @@ class GameStats extends HTMLElement {
 				width: 80%;
 				height: 200px;
 			}
-	`;
+			.stats-table{
+				width: 100%;
+				color : #e67e80;
+				text-align: center;
+			}
+			.stats-table th {
+                background-color: #1d252a;
+                color: rgba(160, 215, 160, 0.9);
+                padding: 10px;
+                border-bottom: 1px solid #323c41;
+                position: sticky; /* Hace que el encabezado sea pegajoso */
+                top: 0; /* Posición del encabezado pegajoso */
+                z-index: 1; /* Asegura que el encabezado esté por encima del contenido */
+            }	
+			th{
+				background-color: #1d252a;
+				color: rgba(160, 215, 160, 0.9);
+				padding: 10px;
+				border-bottom: 1px solid #323c41;
+				position: sticky;
+			}
+			tr:nth-child(even) {
+				background-color: #323c41;
+				
+			}
+			tr:nth-child(odd) {
+				background-color: #2b3339;
+			}
+				`;
+	
 	shadow.appendChild(style);
 	this.container = document.createElement('div');
 	this.container.className = 'GameStatsContainer';
+	
+		// Crear el contenedor de la tabla
+        const tableContainer = document.createElement('div');
+        tableContainer.className = 'table-container';
+
+    	// Crear la estructura de la tabla
+	const table = document.createElement('table');
+	table.className = 'stats-table';
+
+	const thead = document.createElement('thead');
+	const headerRow = document.createElement('tr');
+	const headers = ['Date', 'Player 1', 'Score', 'Player 2', 'Score', 'Winner'];
+	headers.forEach(headerText => {
+		const th = document.createElement('th');
+		th.textContent = headerText;
+		headerRow.appendChild(th);
+	});
+	thead.appendChild(headerRow);
+	table.appendChild(thead);
+
+	const tbody = document.createElement('tbody');
+	tbody.className = 'stats-tbody';
+    table.appendChild(tbody);
+
+	tableContainer.appendChild(table);
+	this.container.appendChild(tableContainer);
+
+	const myDiv = document.createElement('div');
+	myDiv.id = 'myDiv';
+	myDiv.style.width = '100%';
+	myDiv.style.height = '400px';
+	this.container.appendChild(myDiv);
+
 	shadow.appendChild(this.container);
 	}
 
-	async connectedCallback() {
+	
+	
+	async connectedCallback(){
 		let username = localStorage.getItem('username');
 		let token = getCookie('access_token');
 		console.log(token);
 		try {
-			let response = await fetch(`http://localhost:8000/matches/obtainHistory?username=${username}`, {
-		headers: {
-			'Authorization': `Bearer ${token}`
-		}
-			});
-			if (!response.ok) {
-		throw new Error('Error en la peticion');
+			let response = await fetch(`http://localhost:8000/matches/obtainHistory?username=${username}`,{
+			method: 'GET',
+			headers: {
+				'Authorization': `Bearer ${token}`
 			}
-			let stats = await response.json();
-			console.log(stats);
-			this.renderStats(stats);
-		} catch (error) {
-			console.error(error);
+		});
+		if (!response.ok){
+			throw new Error('Error en la peticion');
 		}
+		let gamehistory = await response.json();
+			
+		this.rendergamehistory(gamehistory);
+		this.renderPieChart(gamehistory);
+	}catch(error){
+		console.error('Error en la peticion', error);
+		let nogamehistory = document.createElement('div');
+		nogamehistory.className = 'nogamehistory';
+		nogamehistory.textContent = 'No hay partidas recientes';
 	}
+}
 
-	renderStats(stats) {
-        this.container.innerHTML = '';
+rendergamehistory(gamehistory){
+	const tbody = this.shadowRoot.querySelector('.stats-tbody');
+	tbody.innerHTML = ''; // Clear previous data
 
-        // Create stats container
-        let statsContainer = document.createElement('div');
-        statsContainer.className = 'statsContainer';
+	gamehistory.forEach(match => {
+		const row = document.createElement('tr');
 
-        // Check if there are wins and losses
-        if (stats.wins === 0 && stats.losses === 0) {
-            let noDataMessage = document.createElement('p');
-            noDataMessage.textContent = 'No statistics available';
-            statsContainer.appendChild(noDataMessage);
-        } else {
-            // Create chart container
-            let chartContainer = document.createElement('div');
-            chartContainer.className = 'chart-container';
-            let canvas = document.createElement('canvas');
-            canvas.id = 'statsChart';
-            chartContainer.appendChild(canvas);
-            statsContainer.appendChild(chartContainer);
+		const dateCell = document.createElement('td');
+		const date = new Date(match.date);
+		dateCell.innerHTML = `${date.toLocaleDateString()}`;
+		row.appendChild(dateCell);
 
-            // Render chart
-            this.renderChart(stats.wins, stats.losses);
-        }
+		const player1Cell = document.createElement('td');
+		player1Cell.textContent = match.player1_username;
+		row.appendChild(player1Cell);
 
-        // Check if there are matches
-        if (stats.matches && stats.matches.length > 0) {
-            // Create matches list
-            let matchesList = document.createElement('div');
-            matchesList.className = 'matches-list';
-            stats.matches.forEach(match => {
-                let matchDiv = document.createElement('div');
-                matchDiv.className = 'GameStats-div';
-                matchDiv.textContent = `${match.opponent}: ${match.result}`;
-                matchesList.appendChild(matchDiv);
-            });
-            statsContainer.appendChild(matchesList);
-        } else {
-            let noMatchesMessage = document.createElement('p');
-            noMatchesMessage.textContent = 'No matches available';
-            statsContainer.appendChild(noMatchesMessage);
-        }
+		const player1ScoreCell = document.createElement('td');
+		player1ScoreCell.textContent = match.player1_score;
+		row.appendChild(player1ScoreCell);
 
-        this.container.appendChild(statsContainer);
-    }
-	renderChart(wins, losses) {
-		let ctx = this.shadowRoot.getElementById('statsChart').getContext('2d');
-		new Chart(ctx, {
-		type: 'doughnut',
-		data: {
-			labels: ['Wins', 'Losses'],
-			datasets: [{
-				data: [wins, losses],
-				backgroundColor: ['#4caf50', '#f44336']
-			}]
-		},
-		options: {
-			responsive: true,
-			maintainAspectRatio: false
-		}
+		const player2Cell = document.createElement('td');
+		player2Cell.textContent = match.player2_username;
+		row.appendChild(player2Cell);
+
+		const player2ScoreCell = document.createElement('td');
+		player2ScoreCell.textContent = match.player2_score;
+		row.appendChild(player2ScoreCell);
+
+		const winnerCell = document.createElement('td');
+		winnerCell.textContent = match.winner_username;
+		row.appendChild(winnerCell);
+
+		tbody.appendChild(row);
 	});
 	}
 
-	disconnectedCallback() {}
+	renderPieChart(gamehistory) {
+        const wins = gamehistory.filter(match => match.winner_username === localStorage.getItem("username")).length;
+        const losses = gamehistory.length - wins;
+
+        const data = [{
+            values: [wins, losses],
+            labels: ['Wins', 'Losses'],
+            type: 'pie'
+        }];
+
+        const layout = {
+			title: 'Wins vs Losses',
+            paper_bgcolor: '#2b3339', // Establece el color de fondo del gráfico
+            font: {
+                color: 'rgba(160, 215, 160, 0.9)' // Establece el color del texto
+            }
+        };
+
+        Plotly.newPlot(this.shadowRoot.getElementById('myDiv'), data, layout);
+    }
+	disconectedCallback(){}
 }
 
 window.customElements.define("gamestats-component", GameStats);
@@ -169,3 +231,5 @@ function getCookie(name) {
 	}
 	return null;
 }
+
+https://plotly.com/javascript/bar-charts/
