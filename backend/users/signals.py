68d78cd8e0d3d8087ec4108449bb4f1a4aa8b5f1
Jorge Bortolotti Suarez <jbortolo@car9s4.42barcelona.com>
2024-12-13 10:59:22 +0100

    probando crear un ususario ia en el build

---
+++ backend/users/signals.py
@@ -0,0 +1,8 @@
+from .models import PongUser
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+
+@receiver(post_migrate)
+def create_IA(sender, **kwargs):
+	if not PongUser.objects.filter(username='IA').exists():
+		PongUser.objects.create(username='IA', password="Iapassword", email="ia@gmail.com")
