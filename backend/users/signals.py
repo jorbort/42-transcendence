86708f2f5cd6c3b121b8fe0125c19caae68fff9e
Jorge Bortolotti Suarez <jbortolo@car9s4.42barcelona.com>
2024-12-20 17:43:39 +0100

    arreglos al fetch del partdio en local

--- backend/users/signals.py
+++ backend/users/signals.py
@@ -7,4 +7,4 @@ def create_IA(sender, **kwargs):
 	if not PongUser.objects.filter(username='IA').exists():
 		PongUser.objects.create(username='IA', password="Iapassword", email="ia@gmail.com")
 	if not PongUser.objects.filter(username="localhost").exists():
-		PongUser.objects.filter(username="localhost", password="localPassword", email="localemail@gmail.com")
+		PongUser.objects.create(username="localhost", password="localPassword", email="localemail@gmail.com")
