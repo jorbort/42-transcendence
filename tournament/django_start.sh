ffa017405c41bd1a042d7a544258139eacac350a
jocorrea42 <jrcorrearodriguez@gmail.com>
2024-12-23 17:39:20 +0100

    sleep agregado

--- tournament/django_start.sh
+++ tournament/django_start.sh
@@ -2,12 +2,15 @@
 
 if [ "$DATABASE" = "postgres" ]; then
     while ! nc -z $DATABASE_HOST $SQL_PORT; do
-        sleep 0.1
+        sleep 0.5
     done
 fi
-
+sleep 5
 python3 -m venv .venv && source .venv/bin/activate\
 && pip install -r requirements.txt
+sleep 10
 python3 manage.py makemigrations
+sleep 5
 python3 manage.py migrate
+sleep 5
 gunicorn --bind 0.0.0.0:8001 tournament.wsgi:application
